a943119550fbba39a18cd9225b094facc4fdaadb
BVBFD <lsevina126@gmail.com>
2022-02-26 14:44:54 +0900

    Code Login Signup function and plan how to make backend

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Header from "../../components/header/Header";
 import styles from "./Login.module.css";
 
 const Login = (props) => {
+  const [id, setId] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const idRef = useRef();
+  const pwdRef = useRef();
+
+  const onLogin = (event) => {
+    event.preventDefault();
+    setId(idRef.current.value);
+    setPwd(pwdRef.current.value);
+  };
+  // id 백엔드로 보내서 id를 비교하고, pwd bcrypt compare해서 일치하면
+  // 백엔드에서 id jwt 토큰화 시켜서 전달하고 클라이언트에서는 local db에
+  // 유효기간있는 토큰을 local db에 전달 저장한다.
+
+  console.log(id, pwd);
+
   return (
     <>
       <Header />
-      <form className={styles.loginBox}>
+      <form className={styles.loginBox} onSubmit={onLogin}>
         <div>
           <span>Login</span>
         </div>
         <div className={styles.idBox}>
           <span>ID</span>
-          <input type="text" autoFocus placeholder="Enter your ID" />
+          <input
+            ref={idRef}
+            type="text"
+            autoFocus
+            placeholder="Enter your ID"
+          />
         </div>
         <div className={styles.pwdBox}>
           <span>Password</span>
-          <input type="text" placeholder="Enter your password" />
+          <input ref={pwdRef} type="text" placeholder="Enter your password" />
         </div>
         <button type="submit">login</button>
       </form>
--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Header from "../../components/header/Header";
 import styles from "./Signup.module.css";
 
 const Signup = (props) => {
+  const [id, setId] = useState("");
+  const [email, setEmail] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const idRef = useRef();
+  const emailRef = useRef();
+  const pwdRef = useRef();
+
+  const onSignUp = (event) => {
+    event.preventDefault();
+    setId(idRef.current.value);
+    setEmail(emailRef.current.value);
+    setPwd(pwdRef.current.value);
+  };
+  // 서버 api로 validator 라이브러리 유효성 검사, 서버에서 비밀번호 bcrypt 암호화해서 검사,
+  // 아이디는 jwt 토큰화 시켜서 유효기간 설정후 클라이언트, 서버 정보 교환
+  // jwt 토큰은 클라이언트 로컬 db에 저장 로그아웃 실행시 삭제기능 백엔드, 클라이언트에서 구현할것
+
+  console.log(id, email, pwd);
+
   return (
     <>
       <Header />
-      <form className={styles.signupBox}>
+      <form className={styles.signupBox} onSubmit={onSignUp}>
         <div>
           <span>Sign-up</span>
         </div>
         <div className={styles.idBox}>
           <span>ID</span>
-          <input type="text" autoFocus placeholder="Enter your ID" />
+          <input
+            ref={idRef}
+            type="text"
+            autoFocus
+            placeholder="Enter your ID"
+          />
         </div>
         <div className={styles.emailBox}>
           <span>Email</span>
-          <input type="text" autoFocus placeholder="Enter your ID" />
+          <input
+            ref={emailRef}
+            type="text"
+            autoFocus
+            placeholder="Enter your ID"
+          />
         </div>
         <div className={styles.pwdBox}>
           <span>Password</span>
-          <input type="text" placeholder="Enter your password" />
+          <input ref={pwdRef} type="text" placeholder="Enter your password" />
         </div>
         <button type="submit">Sign-up</button>
       </form>
