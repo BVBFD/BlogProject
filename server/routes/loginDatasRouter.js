762497abe66748a29e3ebfc48243110bf11fdfb3
BVBFD <lsevina126@gmail.com>
2022-02-27 17:12:58 +0900

    Make all api data request urls form on visual studio code and test it on backend console log

--- server/routes/loginDatasRouter.js
+++ server/routes/loginDatasRouter.js
@@ -5,9 +5,19 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
+    console.log(req.body.id, req.body.password);
     res.status(200).json("Login Success!");
   } catch (err) {
     res.status(401).json("invalid id and pwd!");
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    console.log(req.body.id, req.body.password);
+    res.status(201).json("Sign Up!");
+  } catch (err) {
+    res.status(409).json("This Id already existed!");
   }
 });
 
--- server/routes/postsDataRouter.js
+++ server/routes/postsDataRouter.js
@@ -5,9 +5,37 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
+    console.log(req.query.cat);
+    // 쿼리 사용시 (catgories 별로 포스트 뽑기)
     res.status(200).json("Success!");
   } catch (err) {
     res.status(500).josn(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    res.status(201).json("Created!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    console.log(req.params.id);
+    res.status(200).json("Updated!");
+  } catch (err) {
+    res.status(401).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    console.log(req.params.id);
+    res.status(204).json("Deleted No Contents!");
+  } catch (err) {
+    res.status(401).json(err);
   }
 });
 
