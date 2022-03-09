aab366b106053abb519c59998e16490eff4f2149
BVBFD <lsevina126@gmail.com>
2022-03-09 13:39:16 +0900

    keep testing

--- server/app.js
+++ server/app.js
@@ -120,8 +120,6 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-console.log("test!");
-
 mongoose
   .connect(process.env.MONGO_DB_URL)
   .then(() => console.log("Mongo DB Start!"))
