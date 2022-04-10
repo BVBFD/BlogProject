146b189a43e2cbb3206bddec7deb37372d5e6c3a
BVBFD <lsevina126@gmail.com>
2022-04-10 19:54:19 +0900

    Rate Limit reConfig delete keygen

--- server/middleware/rate-limiter.js
+++ server/middleware/rate-limiter.js
@@ -3,5 +3,5 @@ import rateLimit from 'express-rate-limit';
 export default rateLimit({
   windowMs: process.env.RATE_LIMIT_WINDOWMS,
   max: process.env.RATE_LIMIT_MAX,
-  keyGenerator: (req, res) => `${process.env.RATE_LIMIT_KEYGEN}`,
+  // keyGenerator: (req, res) => `${process.env.RATE_LIMIT_KEYGEN}`,
 });
