64962bbd58f150b2d6c8be889a77d422211639ad
BVBFD <lsevina126@gmail.com>
2022-09-05 21:20:19 +0900

    remove .git file in client

---
+++ client/src/pages/post/Post.jsx
@@ -0,0 +1,127 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import Header from '../../components/header/Header';
+import styles from './Post.module.css';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import Write from '../write/Write';
+import { Context } from '../../context/context';
+import axiosInstance from '../../config';
+
+import 'react-quill/dist/quill.snow.css';
+import 'react-quill/dist/quill.core.css';
+import 'react-quill/dist/quill.bubble.css';
+import 'highlight.js/styles/vs2015.css';
+
+const Post = () => {
+  const [post, setPost] = useState({});
+  const location = useLocation();
+  const param = useParams();
+  const [editBtnIndex, setEditBtnIndex] = useState(false);
+  const { id } = useContext(Context);
+  const postTextBoxRef = useRef();
+  const navigate = useNavigate();
+
+  const inputText = () => {
+    return { __html: `${post.text}` };
+  };
+
+  useEffect(async () => {
+    try {
+      const res = await axiosInstance.get(`/posts/${param.id}`);
+      setPost(res.data);
+    } catch (err) {
+      window.alert(err);
+    }
+
+    document
+      .querySelectorAll('.videoImgs')
+      .forEach((img) => img.setAttribute('style', ''));
+  }, [location, param, editBtnIndex]);
+
+  const deletePost = async () => {
+    try {
+      const res = await fetch(`http://37.44.244.229/api/posts/${param.id}`, {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          author: id,
+        }),
+      });
+      res.status === 401 &&
+        window.alert(
+          `${res.statusText} This is private Blog. Onle The Admin can edit!!`
+        );
+      res.status === 204 && navigate('/');
+    } catch (err) {
+      window.alert(err);
+    }
+  };
+
+  useEffect(() => {
+    document
+      .querySelectorAll('img')
+      .forEach((img) => img.setAttribute('crossOrigin', 'anonymous'));
+  }, []);
+
+  return (
+    <section className={styles.postPage}>
+      {!editBtnIndex ? (
+        <>
+          <Header />
+          <div className={styles.postBox}>
+            <div className={styles.postImgTextBox}>
+              <div className={styles.postTitleImgBox}>
+                {post.imgUrl === '' ? (
+                  <img src='../images/postdefaultimg.png' />
+                ) : (
+                  <img crossOrigin='anonymous' src={post.imgUrl} alt='' />
+                )}
+              </div>
+              <div className={styles.postTextBox}>
+                <header className={styles.postHeader}>
+                  <p>
+                    Category: <span>{post.catName}</span>
+                  </p>
+                  <span>{post.title}</span>
+                  <div>
+                    <i
+                      onClick={() => {
+                        if (!editBtnIndex) {
+                          setEditBtnIndex(true);
+                        } else {
+                          setEditBtnIndex(false);
+                        }
+                      }}
+                      class='fa-solid fa-pen-to-square'
+                    ></i>
+                    <i onClick={deletePost} class='fa-solid fa-trash'></i>
+                  </div>
+                </header>
+                <div className={styles.authorAndDate}>
+                  <p>
+                    Author: <span>{post.author}</span>
+                  </p>
+                  <span>{new Date(post.createdAt).toDateString()}</span>
+                </div>
+                <div className='ql-snow'>
+                  <div
+                    class='ql-editor'
+                    ref={postTextBoxRef}
+                    className={styles.postContentText}
+                    dangerouslySetInnerHTML={inputText()}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <Write setEditBtnIndex={setEditBtnIndex} />
+      )}
+    </section>
+  );
+};
+
+export default Post;
