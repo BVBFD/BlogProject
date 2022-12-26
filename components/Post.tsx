c1473829c77fc79a58e4832e762ae1bb7256eeba
BVBFD <lsevina126@gmail.com>
2022-12-26 09:09:41 +0900

    code finished home page

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image';
+import styles from '../styles/Post.module.css';
+
 const Post = () => {
   return (
-    <div>
-      <span>Post</span>
+    <div className={styles.wrapper}>
+      <Image alt='postImg' src='/imgs/kislev.jpg' width={300} height={300} />
+      <div className={styles.content}>
+        <header>Kislev feat.Charina Kathrine</header>
+        <span>Web Dec 14 2022</span>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+          illum neque dolores explicabo facilis corrupti natus dolore, culpa
+          fugiat deserunt, porro dicta consectetur atque voluptatibus
+          doloremque, aut odit nulla nemo?
+        </div>
+      </div>
     </div>
   );
 };
