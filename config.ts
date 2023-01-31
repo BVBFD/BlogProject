b4397f8940ad870c6e4d3e54b935a5ed0c4022fb
BVBFD <lsevina126@gmail.com>
2023-01-31 22:17:14 +0900

    change baseURL for Server

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://api.lsevina126.asia';
-const BASE_URL = 'http://localhost:8800';
+const BASE_URL = 'https://api.lsevina126.asia';
+// const BASE_URL = 'http://localhost:8800';
 // const USER = JSON.parse(localStorage?.getItem('persist:root') as string)?.user;
 // const TOKEN = USER
 //   ? JSON.parse(USER).currentUser?.accessToken?.toString()
