e96cf9fe7421ef7a40623d529c749afcf05278b2
BVBFD <lsevina126@gmail.com>
2023-02-01 12:12:40 +0900

    setPosts on client side for fixing errors just try

--- pages/post/[title].tsx
+++ pages/post/[title].tsx
@@ -7,14 +7,14 @@ import { useRouter } from 'next/router';
 import 'highlight.js/styles/vs2015.css';
 // import { publicRequest } from '../../config';
 import Head from 'next/head';
-// import { CircularProgress } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import { GetServerSidePropsContext } from 'next/types';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/user';
 import dynamic from 'next/dynamic';
 
 const PostPage = ({ ps }: any) => {
-  // const [post, setPost] = useState<any>();
+  const [post, setPost] = useState<any>();
   const [editBtnIndex, setEditBtnIndex] = useState<boolean>(false);
   const router = useRouter();
   const { id } = router.query;
@@ -22,11 +22,10 @@ const PostPage = ({ ps }: any) => {
   const Write = dynamic(() => import('../write'));
 
   useEffect(() => {
-    // const getPostOnClient = async () => {
-    //   const res = await publicRequest.get(`/posts/${id}`);
-    //   setPost(res.data);
-    // };
-    // getPostOnClient();
+    const getPostOnClient = () => {
+      setPost(ps);
+    };
+    getPostOnClient();
 
     document
       .querySelectorAll('.videoImgs')
@@ -38,7 +37,7 @@ const PostPage = ({ ps }: any) => {
   }, [editBtnIndex, id]);
 
   const inputText = () => {
-    return { __html: `${ps.text}` };
+    return { __html: `${post.text}` };
   };
 
   const deletePost = async () => {
@@ -111,62 +110,62 @@ const PostPage = ({ ps }: any) => {
         <script src='https://unpkg.com/babel-standalone@6/babel.min.js'></script>
         <script type='text/babel' src='/my-scripts.js'></script> */}
       </Head>
-      {/* {ps ?  */}
-      <div className={styles.postBox}>
-        <div className={styles.postImgTextBox}>
-          <div className={styles.postTitleImgBox}>
-            <Image
-              src={ps.imgUrl}
-              alt=''
-              width={1920}
-              height={1080}
-              crossOrigin='anonymous'
-            />
-          </div>
-          <div className={styles.postTextBox}>
-            <header className={styles.postHeader}>
-              <p>
-                Category: <span>{ps.catName}</span>
-              </p>
-              <span>{ps.title}</span>
-              <div>
-                <Edit
-                  onClick={() => {
-                    if (!editBtnIndex) {
-                      setEditBtnIndex(true);
-                    } else {
-                      setEditBtnIndex(false);
-                    }
-                  }}
-                />
-                <Delete onClick={deletePost} />
-              </div>
-            </header>
-            <div className={styles.authorAndDate}>
-              <p>
-                Author: <span>{ps.author}</span>
-              </p>
-              <span>{new Date(ps.createdAt).toDateString()}</span>
+      {post ? (
+        <div className={styles.postBox}>
+          <div className={styles.postImgTextBox}>
+            <div className={styles.postTitleImgBox}>
+              <Image
+                src={post.imgUrl}
+                alt=''
+                width={1920}
+                height={1080}
+                crossOrigin='anonymous'
+              />
             </div>
-            <div className='ql-snow'>
-              <div
-                // @ts-ignore
-                class='ql-editor'
-                className={styles.postContentText}
-                dangerouslySetInnerHTML={inputText()}
-              ></div>
+            <div className={styles.postTextBox}>
+              <header className={styles.postHeader}>
+                <p>
+                  Category: <span>{post.catName}</span>
+                </p>
+                <span>{post.title}</span>
+                <div>
+                  <Edit
+                    onClick={() => {
+                      if (!editBtnIndex) {
+                        setEditBtnIndex(true);
+                      } else {
+                        setEditBtnIndex(false);
+                      }
+                    }}
+                  />
+                  <Delete onClick={deletePost} />
+                </div>
+              </header>
+              <div className={styles.authorAndDate}>
+                <p>
+                  Author: <span>{post.author}</span>
+                </p>
+                <span>{new Date(post.createdAt).toDateString()}</span>
+              </div>
+              <div className='ql-snow'>
+                <div
+                  // @ts-ignore
+                  class='ql-editor'
+                  className={styles.postContentText}
+                  dangerouslySetInnerHTML={inputText()}
+                ></div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* : (
-         <div className={styles.circularBox}>
-           <CircularProgress size={60} />
-         </div>
-       )} */}
+      ) : (
+        <div className={styles.circularBox}>
+          <CircularProgress size={60} />
+        </div>
+      )}
     </section>
   ) : (
-    <Write post={ps} setEditBtnIndex={setEditBtnIndex} />
+    <Write post={post} setEditBtnIndex={setEditBtnIndex} />
   );
 };
 
