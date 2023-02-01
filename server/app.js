3413d700aee81f22edd4ce9f2551b6eba7ec15fb
BVBFD <lsevina126@gmail.com>
2023-02-01 18:46:34 +0900

    add getting meta seo tag datas in getPostsById Controller and add origin netlify deployment site

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
-const express = require("express");
-require("express-async-errors");
-const helmet = require("helmet");
-const cors = require("cors");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const multer = require("multer");
-const cloudinary = require("cloudinary");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+require('express-async-errors');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const multer = require('multer');
+const cloudinary = require('cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cookieParser = require('cookie-parser');
 
-const postsDataRouter = require("./routes/postsDataRouter.js");
-const loginDatasRouter = require("./routes/loginDatasRouter.js");
-const ContactDatasModel = require("./models/contactDatasModel.js");
+const postsDataRouter = require('./routes/postsDataRouter.js');
+const loginDatasRouter = require('./routes/loginDatasRouter.js');
+const ContactDatasModel = require('./models/contactDatasModel.js');
 
 dotenv.config();
 
@@ -23,18 +23,18 @@ const cspOptions = {
   directives: {
     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
 
-    "img-src": ["'self'", "data:", `*`],
-    "media-src": [
+    'img-src': ["'self'", 'data:', `*`],
+    'media-src': [
       "'self'",
-      "data:",
+      'data:',
       `https://res.cloudinary.com https://www.youtube.com/embed/`,
     ],
-    "child-src": [
+    'child-src': [
       "'self'",
-      "data:",
+      'data:',
       `https://res.cloudinary.com https://www.youtube.com/embed/`,
     ],
-    "frame-src": ["'self'", "data:", `https://www.youtube.com/embed/`],
+    'frame-src': ["'self'", 'data:', `https://www.youtube.com/embed/`],
   },
 };
 
@@ -46,17 +46,22 @@ app.use(
 
 app.use(
   cors({
-    origin: [`http://localhost:3000`, `https://www.lsevina126.asia`],
+    origin: [
+      `http://localhost:3000`,
+      `https://www.lsevina126.asia`,
+      'https://lsevina126.netlify.app',
+      'https://9671-113-131-240-155.jp.ngrok.io',
+    ],
     optionsSuccessStatus: 200,
     credentials: true,
   })
 );
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(cookieParser());
 
-app.get("/lee", (req, res, next) => {
-  console.log("Hey this is initial test code!");
-  return res.status(200).send(console.log("Success!"));
+app.get('/lee', (req, res, next) => {
+  console.log('Hey this is initial test code!');
+  return res.status(200).send(console.log('Success!'));
 });
 
 cloudinary.config({
@@ -68,8 +73,8 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary.v2,
   params: {
-    folder: "myportfolioblogproject",
-    format: async (req, file) => "gif",
+    folder: 'myportfolioblogproject',
+    format: async (req, file) => 'gif',
     public_id: (req, file) => req.filename,
   },
 });
@@ -77,9 +82,9 @@ const storage = new CloudinaryStorage({
 const videoStorage = new CloudinaryStorage({
   cloudinary: cloudinary.v2,
   params: {
-    folder: "myportfolioblogproject/video",
-    resource_type: "video",
-    format: async (req, file) => "mp4",
+    folder: 'myportfolioblogproject/video',
+    resource_type: 'video',
+    format: async (req, file) => 'mp4',
     public_id: (req, file) => req.filename,
     chunk_size: 8000000,
   },
@@ -88,19 +93,19 @@ const videoStorage = new CloudinaryStorage({
 const upload = multer({ storage: storage });
 const videoUpload = multer({ storage: videoStorage });
 
-app.post("/pic/upload", upload.single("file"), (req, res, next) => {
-  res.header("Cross-Origin-Resource-Policy", "cross-origin");
+app.post('/pic/upload', upload.single('file'), (req, res, next) => {
+  res.header('Cross-Origin-Resource-Policy', 'cross-origin');
   res.status(200).json(req.file.path);
 });
 
-app.post("/video/upload", videoUpload.single("file"), (req, res, next) => {
-  res.header("Cross-Origin-Resource-Policy", "cross-origin");
+app.post('/video/upload', videoUpload.single('file'), (req, res, next) => {
+  res.header('Cross-Origin-Resource-Policy', 'cross-origin');
   res.status(200).json(req.file.path);
 });
 
-app.use("/posts", postsDataRouter);
-app.use("/loginDatas", loginDatasRouter);
-app.post("/contacts", async (req, res, next) => {
+app.use('/posts', postsDataRouter);
+app.use('/loginDatas', loginDatasRouter);
+app.post('/contacts', async (req, res, next) => {
   try {
     const newContact = new ContactDatasModel({
       customerName: req.body.customerName,
@@ -108,7 +113,7 @@ app.post("/contacts", async (req, res, next) => {
       number: req.body.number,
       message: req.body.message,
     });
-    !newContact && res.status(400).json("Bad Request!");
+    !newContact && res.status(400).json('Bad Request!');
     const savedNewContact = await newContact.save();
     console.log(savedNewContact);
     res.status(201).json({ savedNewContact });
@@ -128,9 +133,9 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(process.env.MONGO_DB_URL)
-  .then(() => console.log("Mongo DB Start!"))
+  .then(() => console.log('Mongo DB Start!'))
   .catch((err) => console.error(err));
 
 app.listen(process.env.PORT || 8800, () => {
-  console.log("Backend is running check!");
+  console.log('Backend is running check!');
 });
--- server/controllers/postsDataController.js
+++ server/controllers/postsDataController.js
@@ -1,4 +1,4 @@
-const PostDatasModel = require("../models/postDatasModel.js");
+const PostDatasModel = require('../models/postDatasModel.js');
 
 const getAllPostsAndGetPostsByCatnames = async (req, res, next) => {
   const catName = req.query.cat;
@@ -21,12 +21,24 @@ const getAllPostsAndGetPostsByCatnames = async (req, res, next) => {
 
 const getPostsById = async (req, res, next) => {
   const paramId = req.params.id;
+  const meta = req.query.meta;
+
   try {
-    try {
-      const foundPosts = await PostDatasModel.findById(paramId);
-      res.status(200).json(foundPosts);
-    } catch (err) {
-      res.status(404).json(err);
+    const foundPosts = await PostDatasModel.findById(paramId);
+    if (meta) {
+      try {
+        const { text, catName, author, createdAt, updatedAt, ...others } =
+          foundPosts._doc;
+        res.status(200).json(others);
+      } catch (err) {
+        res.status(404).json(err);
+      }
+    } else {
+      try {
+        res.status(200).json(foundPosts);
+      } catch (err) {
+        res.status(404).json(err);
+      }
     }
   } catch (err) {
     res.status(500).json(err);
@@ -42,7 +54,7 @@ const uploadPost = async (req, res, next) => {
       catName: req.body.catName,
       author: req.body.author,
     });
-    !newPost && res.status(400).json("Bad Request!");
+    !newPost && res.status(400).json('Bad Request!');
     const savedNewPost = await newPost.save();
     console.log(savedNewPost);
     res.status(201).json({ savedNewPost });
@@ -63,7 +75,7 @@ const updatePost = async (req, res, next) => {
       );
       res.status(201).json(updatedPost);
     } else {
-      res.status(401).json("You can update and delete own your posts!");
+      res.status(401).json('You can update and delete own your posts!');
     }
   } catch (err) {
     res.status(401).json(err);
@@ -76,9 +88,9 @@ const deletePost = async (req, res, next) => {
     const foundPost = await PostDatasModel.findById(paramId);
     if (req.body.author === foundPost.author) {
       foundPost.delete();
-      res.status(204).json("The Post has been deleted!");
+      res.status(204).json('The Post has been deleted!');
     } else {
-      res.status(401).json("You can update and delete own your posts!");
+      res.status(401).json('You can update and delete own your posts!');
     }
   } catch (err) {
     res.status(401).json(err);
