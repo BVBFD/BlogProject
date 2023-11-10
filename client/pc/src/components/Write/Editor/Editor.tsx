b67df2fed5d30cfd8b92d78995855c2db2adc45f
lsevina126 <lsevina126@gmail.com>
2023-11-10 17:58:34 +0900

    Feat(color-syntax) : Adding Color Syntax of Editor

--- client/pc/src/components/Write/Editor/Editor.tsx
+++ client/pc/src/components/Write/Editor/Editor.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 import Prism from 'prismjs';
 import { Editor as ToastUIEditor } from '@toast-ui/react-editor';
 import chart from '@toast-ui/editor-plugin-chart';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
-import styles from './Editor.module.scss';
 
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-typescript';
@@ -13,9 +13,13 @@ import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-tsx';
 
 import 'prismjs/themes/prism.css';
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/chart/dist/toastui-chart.css';
+
+import styles from './Editor.module.scss';
 
 const Editor = () => {
   return (
@@ -39,6 +43,7 @@ const Editor = () => {
         plugins={[
           [codeSyntaxHighlight, { highlighter: Prism }],
           [chart, { minWidth: 100, maxWidth: 700, minHeight: 100, maxHeight: 500 }],
+          [colorSyntax, {}],
         ]}
         previewStyle="vertical"
         useCommandShortcut
