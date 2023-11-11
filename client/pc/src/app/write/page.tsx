83abc1d75bfd8f8c3e5645a4d9c5c05edfa898fe
lsevina126 <lsevina126@gmail.com>
2023-11-11 12:53:08 +0900

    Refactor(Editor) : Finish Every Features of Editor and Refactored it, Fixed the dark mode error.

--- client/pc/src/app/write/page.tsx
+++ client/pc/src/app/write/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import styles from './page.module.scss';
 import dynamic from 'next/dynamic';
 
-import styles from './page.module.scss';
-
+// ReferenceError: self is not defined 에러
+// Tui Editor 자체가 서버사이드 렌더링이 아니라 브라우저 렌더링이기 때문에
+// self는 브라우저 환경에서 전역개체인 window객체를 가르키는 말.
+// 즉 서버사이드 렌더링시 브라우저 환경이 아니기 떄문에 dynamic import를 통해서
+// 브라우저 환경에 접속하고 난 다음 import 시켜줘야 에러가 발생하지 않음.
 const DynamicEditor = dynamic(() => import('src/components/Write/Editor/Editor'), { ssr: false });
 
 const Write = () => {
--- client/pc/src/components/Write/Editor/Editor.tsx
+++ client/pc/src/components/Write/Editor/Editor.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useContext } from 'react';
 import Prism from 'prismjs';
 
 import { Editor as ToastUIEditor } from '@toast-ui/react-editor';
@@ -26,64 +26,74 @@ import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-sy
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import '@toast-ui/chart/dist/toastui-chart.css';
 import '@toast-ui/editor-plugin-table-merged-cell/dist/toastui-editor-plugin-table-merged-cell.css';
+import { ThemeContext } from 'src/common/context/ThemeContext';
 
 const Editor = () => {
+  const { mode } = useContext(ThemeContext);
+
+  const codeSyntaxHighlightOption = { highlighter: Prism };
+  const chartOption = { minWidth: 100, maxWidth: 700, minHeight: 100, maxHeight: 500 };
+  const colorSyntaxPresetOption = {
+    preset: [
+      '#333333',
+      '#666666',
+      '#FFFFFF',
+      '#EE2323',
+      '#F89009',
+      '#009A87',
+      '#006DD7',
+      '#8A3DB6',
+      '#781B33',
+      '#5733B1',
+      '#953B34',
+      '#FFC1C8',
+      '#FFC9AF',
+      '#9FEEC3',
+      '#99CEFA',
+      '#C1BEF9',
+    ],
+  };
+
+  const hookOption = {
+    addImageBlobHook() {
+      // dev test
+      // fileBlob, imgUrlCallback as params inside of callback function
+      // console.log(fileBlob);
+      // console.log(
+      //   imgUrlCallback(
+      //     'https://res.cloudinary.com/dewa3t2gi/image/upload/v1698394328/myportfolioblogproject/sfbu4gzzxjrg584kjlpc.gif'
+      //   )
+      // );
+    },
+  };
+
+  const toolbarOption = [
+    // 툴바 옵션 설정
+    ['heading', 'bold', 'italic', 'strike'],
+    ['hr', 'quote'],
+    ['ul', 'ol', 'task', 'indent', 'outdent'],
+    ['table', 'image', 'link'],
+    ['code', 'codeblock'],
+  ];
+
+  // toastui-editor-dark toastui-editor-defaultUI
   return (
-    <div className={styles.container}>
+    // dark 다크모드 속성이 먹히지 않음.. 그래서 이렇게 설정하여 구현
+    <div className={mode === 'light' ? `${styles.container}` : `${styles.container} toastui-editor-dark`}>
       <ToastUIEditor
         height="80vh"
-        hooks={{
-          addImageBlobHook() // fileBlob, imgUrlCallback as params inside of callback function
-          {
-            // dev test
-            // console.log(fileBlob);
-            // console.log(
-            //   imgUrlCallback(
-            //     'https://res.cloudinary.com/dewa3t2gi/image/upload/v1698394328/myportfolioblogproject/sfbu4gzzxjrg584kjlpc.gif'
-            //   )
-            // );
-          },
-        }}
+        hooks={hookOption}
         initialEditType="markdown"
         initialValue="Tell Me Your Story..."
         plugins={[
-          [codeSyntaxHighlight, { highlighter: Prism }],
-          [chart, { minWidth: 100, maxWidth: 700, minHeight: 100, maxHeight: 500 }],
-          [
-            colorSyntax,
-            {
-              preset: [
-                '#333333',
-                '#666666',
-                '#FFFFFF',
-                '#EE2323',
-                '#F89009',
-                '#009A87',
-                '#006DD7',
-                '#8A3DB6',
-                '#781B33',
-                '#5733B1',
-                '#953B34',
-                '#FFC1C8',
-                '#FFC9AF',
-                '#9FEEC3',
-                '#99CEFA',
-                '#C1BEF9',
-              ],
-            },
-          ],
+          [codeSyntaxHighlight, codeSyntaxHighlightOption],
+          [chart, chartOption],
+          [colorSyntax, colorSyntaxPresetOption],
           [tableMergedCell, {}],
         ]}
         previewStyle="vertical"
-        theme="light" // dark 다크모드
-        toolbarItems={[
-          // 툴바 옵션 설정
-          ['heading', 'bold', 'italic', 'strike'],
-          ['hr', 'quote'],
-          ['ul', 'ol', 'task', 'indent', 'outdent'],
-          ['table', 'image', 'link'],
-          ['code', 'codeblock'],
-        ]}
+        theme="light" // 다크모드 속성 먹히지 않음. 라이브러리 문제.
+        toolbarItems={toolbarOption}
         usageStatistics={false} // 통계 수집 거부
         useCommandShortcut={false}
         // hideModeSwitch={true} // 하단 숨기기
