e038fa94d2ab6126d1956eeac2f632a04bd47c65
lsevina126 <lsevina126@gmail.com>
2023-11-12 17:23:35 +0900

    ES-Lint(contact) : ES-Lint re-order the code.

--- client/pc/src/app/contact/page.tsx
+++ client/pc/src/app/contact/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import styles from './page.module.scss';
+
 import Image from 'next/image';
-import ContactImg from '../../../public/imgs/contact.gif';
+
 import Button from 'src/common/Button/Button';
+
+import ContactImg from '../../../public/imgs/contact.gif';
+
+import styles from './page.module.scss';
 
 const Contact = () => {
   return (
@@ -10,28 +14,28 @@ const Contact = () => {
       <header>Contact</header>
       <main>
         <div className={styles.imgBox}>
-          <Image src={ContactImg} alt="contact_img" fill />
+          <Image alt="contact_img" fill src={ContactImg} />
         </div>
         <form className={styles.infoBox}>
           <h3>Name</h3>
-          <input defaultValue={''} placeholder="Enter your name..." type="text" />
+          <input defaultValue="" placeholder="Enter your name..." type="text" />
 
           <h3>Email</h3>
-          <input defaultValue={''} placeholder="Enter your Email..." type="email" />
+          <input defaultValue="" placeholder="Enter your Email..." type="email" />
 
           <h3>Number</h3>
-          <input defaultValue={''} placeholder="Enter your Number..." type="number" />
+          <input defaultValue="" placeholder="Enter your Number..." type="number" />
 
           <h3>Message</h3>
-          <textarea defaultValue={''} placeholder="Type your message to me..." />
+          <textarea defaultValue="" placeholder="Type your message to me..." />
 
           <Button
             className={styles.customCSSButton}
             fontSize="1.1rem"
-            width="15vh"
             height="2.6rem"
+            href=""
             text="Send Email"
-            href={''}
+            width="15vh"
           />
         </form>
       </main>
