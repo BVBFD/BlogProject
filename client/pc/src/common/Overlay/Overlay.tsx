63f5c3e492b82bbec9a661eee95940354c82b744
lsevina126 <lsevina126@gmail.com>
2023-11-13 11:45:27 +0900

    Feat(props-Overlay) : onClick and zIndex props added into Overlay module.

--- client/pc/src/common/Overlay/Overlay.tsx
+++ client/pc/src/common/Overlay/Overlay.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import styles from './Overlay.module.scss';
 
-const Overlay = ({ children }) => {
-  return <div className={styles.modalOverlay}>{children}</div>;
+const Overlay = ({ children, onClick, zIndex }) => {
+  return (
+    <div className={`${styles.modalOverlay} z-[${zIndex}]`} onClick={onClick} role="presentation">
+      {children}
+    </div>
+  );
+};
+
+Overlay.defaultProps = {
+  children: undefined,
+  onClick: undefined,
+  zIndex: 999999,
 };
 
 export default Overlay;
