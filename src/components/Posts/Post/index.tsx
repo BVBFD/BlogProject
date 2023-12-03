b64a12381f9dec0356a0047ae73d7af0813bc9fd
lsevina126 <lsevina126@gmail.com>
2023-12-04 01:46:56 +0900

    Fix(img-load):Change the method of loading imgs

--- src/components/Posts/Post/index.tsx
+++ src/components/Posts/Post/index.tsx
@@ -26,10 +26,17 @@ const Post = ({ post, setOnProgress }: { post: PostType; setOnProgress: Dispatch
           alt="postImg"
           crossOrigin="anonymous"
           fill
-          loading="lazy"
+          // loading="eager":
+          // 페이지가 로드될 때 모든 이미지가 미리 다운로드되므로
+          // 초기에 화면에 이미지가 나타나는 속도가 빠릅니다.
+
+          // loading="lazy": 페이지가 로드될 때 이미지를 처음에 다운로드하지 않고,
+          // 사용자가 스크롤하거나 특정 위치에 도달할 때 필요한 이미지만 다운로드되기
+          // 때문에 초기에 로딩 속도가 빠릅니다.
+          // 페이지의 모든 이미지가 필요하지 않다면 자원을 절약할 수 있습니다.
+          loading="eager"
           objectFit="contain"
           onLoad={() => setOnProgress(false)}
-          onLoadingComplete={() => setOnProgress(false)}
           onLoadStart={() => setOnProgress(true)}
           src={post?.imgUrl}
         />
