c5a629c3757ffc835c8757bf155205fdb6dcc032
lsevina126 <lsevina126@gmail.com>
2023-12-04 23:46:28 +0900

    Feat(prisma):Setting Prisma and mongodb

--- client/pc/src/app/api/auth/[...nextauth]/route.ts
+++ client/pc/src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,13 @@ import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
 import KakaoProvider from 'next-auth/providers/kakao';
 
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
