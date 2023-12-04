66a79dbf84fefabf7b8818032566a14781f7949b
lsevina126 <lsevina126@gmail.com>
2023-12-05 00:15:10 +0900

    Fix(image-loading):Fixed the image-loading properly

--- src/components/Posts/Post/index.tsx
+++ src/components/Posts/Post/index.tsx
@@ -19,34 +19,42 @@ const Post = ({ post, setOnProgress }: { post: PostType; setOnProgress: Dispatch
   //   return { __html: `${post?.text}` };
   // };
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.imgBox}>
-        <Image
-          alt="postImg"
-          crossOrigin="anonymous"
-          fill
-          // loading="eager":
-          // 페이지가 로드될 때 모든 이미지가 미리 다운로드되므로
-          // 초기에 화면에 이미지가 나타나는 속도가 빠릅니다.
+  if (!post) {
+    setOnProgress(true);
+  } else {
+    setOnProgress(false);
+  }
 
-          // loading="lazy": 페이지가 로드될 때 이미지를 처음에 다운로드하지 않고,
-          // 사용자가 스크롤하거나 특정 위치에 도달할 때 필요한 이미지만 다운로드되기
-          // 때문에 초기에 로딩 속도가 빠릅니다.
-          // 페이지의 모든 이미지가 필요하지 않다면 자원을 절약할 수 있습니다.
-          loading="eager"
-          objectFit="contain"
-          onLoad={() => setOnProgress(false)}
-          onLoadStart={() => setOnProgress(true)}
-          src={post?.imgUrl}
-        />
+  return (
+    post && (
+      <div className={styles.wrapper}>
+        <div className={styles.imgBox}>
+          <Image
+            alt="postImg"
+            crossOrigin="anonymous"
+            fill
+            // loading="eager":
+            // 페이지가 로드될 때 모든 이미지가 미리 다운로드되므로
+            // 초기에 화면에 이미지가 나타나는 속도가 빠릅니다.
+
+            // loading="lazy": 페이지가 로드될 때 이미지를 처음에 다운로드하지 않고,
+            // 사용자가 스크롤하거나 특정 위치에 도달할 때 필요한 이미지만 다운로드되기
+            // 때문에 초기에 로딩 속도가 빠릅니다.
+            // 페이지의 모든 이미지가 필요하지 않다면 자원을 절약할 수 있습니다.
+            loading="eager"
+            objectFit="contain"
+            onLoad={() => setOnProgress(false)}
+            onLoadStart={() => setOnProgress(true)}
+            src={post.imgUrl}
+          />
+        </div>
+        <div className={styles.content}>
+          <header>{post.title}</header>
+          <span>{new Date(post.createdAt).toDateString()}</span>
+          {/* <div dangerouslySetInnerHTML={inputText()} /> */}
+        </div>
       </div>
-      <div className={styles.content}>
-        <header>{post?.title}</header>
-        <span>{new Date(post?.createdAt).toDateString()}</span>
-        {/* <div dangerouslySetInnerHTML={inputText()} /> */}
-      </div>
-    </div>
+    )
   );
 };
 
