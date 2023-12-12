fbd824b1b255327881d165922e6b12cd76f62ad4
BVBFD <lsevina126@gmail.com>
2023-12-12 19:14:55 +0900

    For(while):Re-connect logic to mongodb for fast initial connect to mongodb

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,18 +1,29 @@
-import mongoose from 'mongoose';
+async function dbConnect() {
+  let maxRetries = 10;
+  let currentRetry = 0;
 
-function dbConnect() {
-  return mongoose.connect(process.env.MONGO_DB_URL, {
-    dbName: 'myFirstDatabase',
-    connectTimeoutMS: 1000,
-    serverSelectionTimeoutMS: 1000,
-    maxPoolSize: 10,
-    minPoolSize: 1,
-  });
+  while (currentRetry < maxRetries) {
+    try {
+      await mongoose.connect(process.env.MONGO_DB_URL, {
+        dbName: 'myFirstDatabase',
+        connectTimeoutMS: 1000,
+        serverSelectionTimeoutMS: 1000,
+        maxPoolSize: 10,
+        minPoolSize: 1,
+      });
+
+      console.log('MongoDB connected successfully!!');
+      break;
+    } catch (error) {
+      currentRetry++;
+      console.log('MongoDB connect failed!!');
+      // Add some delay before retrying
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
+
+  if (currentRetry === maxRetries) {
+    // Handle the case where max retries are reached
+    console.error('Max retries reached. MongoDB connection failed.');
+  }
 }
-
-function dbDisConnect() {
-  return mongoose.disconnect();
-}
-
-export { dbDisConnect };
-export default dbConnect;
