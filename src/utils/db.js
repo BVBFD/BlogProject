b56cbb59ecd181cc04355c8fe44483ac7a2bb640
lsevina126 <lsevina126@gmail.com>
2023-12-24 10:12:53 +0900

    DB(Connect-Config):700ms as DB Connect-Config

--- src/utils/db.js
+++ src/utils/db.js
@@ -8,8 +8,8 @@ async function dbConnect() {
     try {
       await mongoose.connect(`${process.env.MONGO_DB_URL}`, {
         dbName: 'myFirstDatabase',
-        connectTimeoutMS: 600,
-        serverSelectionTimeoutMS: 600,
+        connectTimeoutMS: 700,
+        serverSelectionTimeoutMS: 700,
         maxPoolSize: 10,
         minPoolSize: 1,
       });
@@ -20,7 +20,7 @@ async function dbConnect() {
       currentRetry++;
       console.log('MongoDB connect failed!!');
       // Add some delay before retrying
-      await new Promise((resolve) => setTimeout(resolve, 600));
+      await new Promise((resolve) => setTimeout(resolve, 700));
     }
   }
 
