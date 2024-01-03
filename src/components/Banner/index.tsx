9f53222194b7944999311fce62a5353de9d18da5
lsevina126 <lsevina126@gmail.com>
2024-01-04 08:00:21 +0900

    Edit(banner 이미지):배너 이미지 레이아웃 위치 및 패딩 수정

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,7 +4,9 @@ import styles from './index.module.scss';
 const Banner = () => {
   return (
     <div className={styles.bannerBox}>
-      <Image alt="banner" fetchPriority="high" fill loading="lazy" quality={5} src="/imgs/banner.png" />
+      <div className={styles.imgBox}>
+        <Image alt="banner" fetchPriority="high" fill loading="lazy" quality={5} src="/imgs/banner.png" />
+      </div>
     </div>
   );
 };
