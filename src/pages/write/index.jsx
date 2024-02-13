e68ffaf7fd12f2cce58ebfc59703b3caa049cb0f
lsevina126 <lsevina126@gmail.com>
2024-02-13 11:42:48 +0900

    Edit(editor): quill editor 하단에 여백을 주어서 제일 밑부분 가독성 편하게 해주었음

--- src/pages/write/index.jsx
+++ src/pages/write/index.jsx
@@ -405,7 +405,7 @@ const Write = ({ post, editBtnIndex, setEditBtnIndex }) => {
           forwardedRef={editorRef}
           modules={modules}
           onChange={setValue}
-          style={{ width: '100%', height: '90vh' }}
+          style={{ width: '100%', height: '90vh', marginBottom: '10vh' }}
           theme="snow"
         />
         {!isFetching ? (
