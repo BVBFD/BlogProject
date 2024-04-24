547e9262fedf2dbee5a729b8c838283ee4245857
lsevina126 <lsevina126@gmail.com>
2024-04-24 20:31:00 +0900

    Fix(config): setting config file as baseURL

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { mutate } from 'swr';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
-const TEST_BASE_URL = `${process.env.NEXT_PUBLIC_TEST_BASE_URL}`;
-const NEXT_API_BASE_URL = `${process.env.NEXT_PUBLIC_NEXT_API_BASE_URL}`;
+// const TEST_BASE_URL = `${process.env.NEXT_PUBLIC_TEST_BASE_URL}`;
+// const NEXT_API_BASE_URL = `${process.env.NEXT_PUBLIC_NEXT_API_BASE_URL}`;
 const DEFAULT_TIMEOUT = 30000;
 const Origin = 'http://localhost:3000' || 'https://lsevina126.netlify.app';
 
 export const publicRequest = axios.create({
-  baseURL: BASE_URL || TEST_BASE_URL || NEXT_API_BASE_URL,
+  baseURL: BASE_URL,
   withCredentials: true,
   timeout: DEFAULT_TIMEOUT,
   headers: {
@@ -28,11 +28,11 @@ interface SwrUrlType {
 
 export const getSwrUrl = ({ currentPageNum, searchText, catName, postId }: SwrUrlType) => {
   if (postId) {
-    return `${BASE_URL || TEST_BASE_URL || NEXT_API_BASE_URL}/posts/${postId}`;
+    return `${BASE_URL}/posts/${postId}`;
   }
-  return `${BASE_URL || TEST_BASE_URL || NEXT_API_BASE_URL}/posts?${currentPageNum && `page=${currentPageNum}`}${
-    searchText && `&text=${searchText}`
-  }${catName && `&cat=${catName}`}`;
+  return `${BASE_URL}/posts?${currentPageNum && `page=${currentPageNum}`}${searchText && `&text=${searchText}`}${
+    catName && `&cat=${catName}`
+  }`;
 };
 
 export const fetcher = (url: string) => publicRequest.get(url).then((res) => res.data);
